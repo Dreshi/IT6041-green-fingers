9553995e65c1a2f93c3ade27938b5abe71db7840
Dreshi <73090979+Dreshi@users.noreply.github.com>
2022-11-30 10:59:24 +1300

    add shipping and promo code to cart

--- front_end/pages/api/stripe.js
+++ front_end/pages/api/stripe.js
@@ -11,8 +11,12 @@ export default async function handler(req, res) {
                 mode: 'payment',
                 payment_method_types: ["card"],
                 shipping_address_collection: {
-                    allowed_countries: ["US", "CA"],
+                    allowed_countries: ["US", "CA", "GB", "RO", "NZ", "AU"],
                 },
+                shipping_options: [
+                    {shipping_rate: 'shr_1M9bRrJX0FYnN85ms0qZeT00'}, {shipping_rate: 'shr_1M9bmMJX0FYnN85mXcIXVLMi'}
+                ],
+                allow_promotion_codes: true,
                 line_items: req.body.map(item => {
                     return{
                         price_data: {
@@ -22,6 +26,10 @@ export default async function handler(req, res) {
                                 images: [item.image.data.attributes.formats.thumbnail.url],
                             },
                             unit_amount: item.price * 100,
+                        },
+                        adjustable_quantity: {
+                            enabled: true,
+                            minimum: 1
                         },
                         quantity: item.quantity,
                     };
