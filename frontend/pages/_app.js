6dcffe4e504c8bca305189ca4d2d39c409bc6cbb
ztur211 <ztur211@mywhitecliffe.com>
2022-12-02 11:14:31 +1300

    meilisearch search

---
+++ frontend/pages/_app.js
@@ -0,0 +1,25 @@
+import "../styles/globals.css";
+import {Provider, createClient} from "urql";
+import Nav from "../components/Nav";
+import { StateContext } from "../lib/context";
+import { UserProvider } from "@auth0/nextjs-auth0";
+import { Toaster } from "react-hot-toast";
+import React from 'react';
+
+const client = createClient({ url: process.env.NEXT_PUBLIC_BACKEND_API });
+
+function MyApp({ Component, pageProps }) {  
+  return (
+    <UserProvider>
+      <StateContext>
+        <Provider value={client}>
+          <Toaster />
+          <Nav />
+          <Component {...pageProps} />
+        </Provider>  
+      </StateContext>
+    </UserProvider>
+  );
+}
+
+export default MyApp;
