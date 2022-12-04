ddf28bf1ae2e2f47f251969645d2780aba7cf488
ppc99999 <ppc99999@gmail.com>
2022-12-05 06:00:35 +1300

    stlying index

--- frontend/styles/Gallery.js
+++ frontend/styles/Gallery.js
@@ -3,5 +3,5 @@ import styled from "styled-components";
 export const Gallery = styled.div`
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
-    grid-gap: 2.2rem;
+    grid-gap: 2.8rem;
 `;
