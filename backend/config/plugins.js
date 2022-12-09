f362fe8ad82c317c767bb0bc35c84436e656300b
antonKuk <akuk211@mywhitecliffe.com>
2022-12-09 18:57:34 +1300

    fixed gitignore

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,26 +1,18 @@
 module.exports = ({ env }) => ({
-    // ...
-    meilisearch: {
-      config: {
-        // Your meili host
-        host: "http://localhost:7700",
-        // Your master key or private key
-        apiKey: env('MEILI_MASTER_KEY'),
-      }
-    },
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-          upload: {},
-          delete: {},
-        },
+  // ...
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
       },
     },
-    // ...
-  });
+  },
+  // ...
+});
